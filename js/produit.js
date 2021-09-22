69c6ab9fa61bd9b187cc4db36cf577e185f7c614
sachi86 <sachi8676@gmail.com>
2021-09-22 17:41:13 +0200

    affichage produit sur page produit CSS

--- js/produit.js
+++ js/produit.js
@@ -10,26 +10,31 @@ const Id = urlSearchParams.get("id");
 console.log(Id);
 
 //affichage du produit qui à été selectionné par l'id avec la méthode fetch
-
-let productSelected =  fetch(`http://localhost:3000/api/teddies/${Id}`)
-    .then(data => data.json())
-        let product = new Product(jsonProduct); 
+fetch(`http://localhost:3000/api/teddies/${Id}`)
+    .then(response => response.json())
+    .then(dataProduct => {
+        let product = new Product(dataProduct); 
         document.querySelector(".productCart").innerHTML += `<article class="productCardId">
                                     <div class="imageProductId">
                                         <img class="imgUrlId" src="${product.imageUrl}" alt="photo du produit">
                                     </div>
                                     <div class="descriptionProductId">
                                         <h3 class="nameId">${product.name}</h3>
-                                        <form>
-                                            <label for="pays">Couleurs</label><br />
+                                        <form class="productFormId">
+                                            <label for="colors" class="colorsId">Couleur :</label>
                                             <select name="colors" id="colors">
-                                                <option value="colors">${product.colors}</option>
+                                                <option value="colors">${product.colors[0]}</option>
+                                                <option value="colors">${product.colors[1]}</option>
+                                                <option value="colors">${product.colors[2]}</option>
+                                                <option value="colors">${product.colors[3]}</option>
                                             </select>
                                         </form>
                                         <p class="descriptionId">${product.description}</p>
-                                        <p class="priceId">${product.getConvertedPrice}</p>
+                                        <p class="priceId">${product.getConvertedPrice()}</p>
+                                        <button type="submit" class="btnProduct">Ajouter au panier</button>
                                     </div>
                                 </article>`
+    })
         .catch(function(err) {
             alert("Désolé nous n'avons pas pu afficher le petit ourson Orinoco que vous voulez-voir! Si vous rencontrez un problème contactez nous! Erreur : " +err);
 });
