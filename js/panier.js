1d01afb2e62e6b414da36435e087b957425d0d8a
Christelle <sachi8676@gmail.com>
2021-09-28 13:47:08 +0200

    affichage des produits dans le panier

--- js/panier.js
+++ js/panier.js
@@ -20,3 +20,40 @@ function saveProductBasket(listProductBasket){
     localStorage.setItem("ProductBasket", JSON.stringify(listProductBasket));
 }
 
+// Constante pour l'affichage et la récupération des produit dans le local storage
+const productStorageBasket = getProductBasket();
+const api = "http://localhost:3000/api/teddies/";
+
+//Récupération des produit dans le local storage pour l'affichage du panier
+function getViewProductBasket(){
+    for(let key of productStorageBasket){
+        console.log(key);
+        fetch(api + key).then(response => response.json())
+        .then(dataProductBasket =>{
+            let productBasket = new Product(dataProductBasket);
+            document.querySelector("#basket").innerHTML += `<tr>
+                                                                <td class="photoBasket"><img src="${productBasket.imageUrl}" alt="photo du produit" class="photoProductBasket"</td>
+                                                                <td>${productBasket.name}</td>
+                                                                <td>${productBasket._id}</td>
+                                                                <td>1</td>
+                                                                <td>${productBasket.getConvertedPrice()}</td>
+                                                            </tr>`
+        })
+    }
+};
+
+//Affichage du panier
+function wieuProductBasket(){
+    console.log(productStorageBasket);
+    if(productStorageBasket.length > 0){
+        getViewProductBasket();
+    }else{
+        document.querySelector("#basket").innerHTML += `<p>Votre panier est vide! Et si vous alliez voir nos produits ? Nos oursons n'attandent plus que vous !</p>`
+    }
+}
+
+const reducer = (accumulator,currentValue) => accumulator + currentValue;
+//fonction executé apres que la page soit chargée
+window.addEventListener("load", function() { //attente de la fin de chargement de la page pour appeler les fonctions
+wieuProductBasket();
+});
