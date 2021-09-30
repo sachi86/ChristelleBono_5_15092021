50eb62052ae84d23f8a9a51ae5f07fabe2ca2a24
Christelle <sachi8676@gmail.com>
2021-09-30 15:52:14 +0200

    confirmation de commande avec id order

---
+++ frontend/js/confirmation.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function() { 
+        let orderBasket = JSON.parse(localStorage.order);
+        let orderBasketId = orderBasket.orderId;
+        let orderBasketProducts = orderBasket.products;
+        let totalBasket = 0;
+
+        for(i in orderBasketProducts){
+            totalBasket += orderBasketProducts[i].price;
+            console.log(totalBasket);
+        }
+        document.querySelector("#IdOrder").innerHTML += `${orderBasketId}`;
+        document.querySelector("#PriceOrder").innerHTML += `${convertDisplayPrice(totalBasket)}`;
+        localStorage.clear();
+        products = [];
+    });
